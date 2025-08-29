351b5a33fc8cf5cd32fa4eafe81a6a153aba674c
MatteoOlarte <mateo-cerquerao@unilibre.edu.co>
2025-08-29 15:09:47 -0500

    Borrando duplicado

--- Src/Core/Data/DatabaseContext.cs
+++ Src/Core/Data/DatabaseContext.cs
@@ -1,29 +1,20 @@
 using EAAB.AddIn.Src.Core.Data.Common;
 
+using EAABAddIn.Src.Core.Entities;
+
 using Microsoft.EntityFrameworkCore;
 
 namespace EAAB.AddIn.Src.Core.Data;
 
-/// <summary>
-/// Clase base para el contexto de Entity Framework
-/// </summary>
 public class DatabaseContext : DbContext
 {
     private readonly IDatabaseStrategy _databaseStrategy;
 
-    /// <summary>
-    /// Constructor que recibe la estrategia de base de datos a utilizar
-    /// </summary>
-    /// <param name="databaseStrategy">Estrategia de base de datos</param>
     public DatabaseContext(IDatabaseStrategy databaseStrategy)
     {
         _databaseStrategy = databaseStrategy;
     }
 
-    /// <summary>
-    /// Configura las opciones del contexto utilizando la estrategia de base de datos
-    /// </summary>
-    /// <param name="optionsBuilder">Builder para configurar opciones</param>
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         if (!optionsBuilder.IsConfigured)
@@ -34,12 +25,14 @@ public class DatabaseContext : DbContext
         base.OnConfiguring(optionsBuilder);
     }
 
-    /// <summary>
-    /// Configura el modelo de la base de datos
-    /// </summary>
-    /// <param name="modelBuilder">Builder para configurar el modelo</param>
+    public DbSet<AddressLexEntity> AddressLexEntities { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<AddressLexEntity>(entity =>
+        {
+            
+        });
         base.OnModelCreating(modelBuilder);
     }
 }
