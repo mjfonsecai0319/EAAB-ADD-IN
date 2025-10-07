381ca185d098a6552ffe470b8ab261f8217862c4
MatteoOlarte <mateoc09+2@gmail.com>
2025-10-07 12:08:48 -0500

    Fix: Mejora tooltips de botones en Config.daml para mayor claridad y agrega tooltips para importar y unir polígonos.

---
+++ Src/Presentation/View/Buttons/ImportPolygonButton.cs
@@ -0,0 +1,11 @@
+using ArcGIS.Desktop.Framework.Contracts;
+
+namespace EAABAddIn.Src.Presentation.View.Buttons;
+
+public class ImportPolygonButton : Button
+{
+    protected override void OnClick()
+    {
+        // Implement the logic for importing a polygon
+    }
+}
---
+++ Src/Presentation/View/Buttons/UnionPolygonsButton.cs
@@ -0,0 +1,11 @@
+using ArcGIS.Desktop.Framework.Contracts;
+
+namespace EAABAddIn.Src.Presentation.View.Buttons;
+
+public class UnionPolygonsButton : Button
+{
+    protected override void OnClick()
+    {
+        // Implement the logic for unioning polygons
+    }
+}
